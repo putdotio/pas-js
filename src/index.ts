4c56a778b145e170417d023dcc0af593a6e07059
Altay Aydemir <altay.aydemir@gmail.com>
2020-01-12 15:24:01 +0100

    add cookie options

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
 import merge from 'deepmerge'
+import Cookies from 'js-cookie'
 import log from 'loglevel'
 import pkg from '../package.json'
 
 interface IPutioAnalyticsClientOptions {
   loglevel: log.LogLevelDesc
-  domain: string
+  cookies?: {
+    domain: string
+    expires: number
+    userKey: string
+    eventQueueKey: string
+  }
 }
 
 class PutioAnalyticsClient {
   public static DEFAULT_OPTIONS: IPutioAnalyticsClientOptions = {
     loglevel: 'WARN',
-    domain: '.put.io',
+    cookies: {
+      domain: '.put.io',
+      userKey: 'pas_js_user',
+      eventQueueKey: 'pas_js_event_queue',
+      expires: 365,
+    },
   }
 
   public options: IPutioAnalyticsClientOptions
   public logger: log.Logger
   public version = pkg.version
 
-  constructor(options: IPutioAnalyticsClientOptions) {
+  constructor(options?: IPutioAnalyticsClientOptions) {
     this.options = merge(options, {})
+
     this.logger = log.noConflict()
     this.logger.setLevel(this.options.loglevel)
   }
