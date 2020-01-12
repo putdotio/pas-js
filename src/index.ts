84e939d319d0da5326a72dd7afe8d3aa1d7949f0
Altay Aydemir <altay.aydemir@gmail.com>
2020-01-12 15:11:30 +0100

    add domain attribute to options

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import merge from 'deepmerge'
 import log from 'loglevel'
+import pkg from '../package.json'
 
 interface IPutioAnalyticsClientOptions {
   loglevel: log.LogLevelDesc
+  domain: string
 }
 
 class PutioAnalyticsClient {
   public static DEFAULT_OPTIONS: IPutioAnalyticsClientOptions = {
     loglevel: 'WARN',
+    domain: '.put.io',
   }
 
   public options: IPutioAnalyticsClientOptions
   public logger: log.Logger
+  public version = pkg.version
 
   constructor(options: IPutioAnalyticsClientOptions) {
     this.options = merge(options, {})
