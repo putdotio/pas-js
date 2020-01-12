f3a00ca2a551dd675062fc6b15da16976215ca33
Altay Aydemir <altay.aydemir@gmail.com>
2020-01-12 16:28:29 +0100

    add tests for cache utility

---
+++ src/cache.spec.ts
@@ -0,0 +1,30 @@
+import Cookies from 'js-cookie'
+import createCache from './cache'
+
+jest.mock('js-cookie', () => {
+  return {
+    set: jest.fn(),
+    getJSON: jest.fn(),
+    remove: jest.fn(),
+  }
+})
+
+describe('cache utility', () => {
+  const options = { domain: '.put.io', expires: 365 }
+  const cache = createCache(options)
+
+  it('calls Cookies.set for saving data to cookies', () => {
+    cache.set('key', 'value')
+    expect(Cookies.set).toBeCalledWith('key', 'value', options)
+  })
+
+  it('calls Cookies.getJSON for reading data from cookies', () => {
+    cache.get('key')
+    expect(Cookies.getJSON).toBeCalledWith('key')
+  })
+
+  it('calls Cookes.remove to remove data from cookies', () => {
+    cache.clear('key')
+    expect(Cookies.remove).toBeCalledWith('key', { domain: options.domain })
+  })
+})
