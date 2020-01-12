1209bbe575d81bb264a30227ba5b6d2c36dbb759
Altay Aydemir <altay.aydemir@gmail.com>
2020-01-12 15:06:06 +0100

    configure loglevel for logs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-class Pas {}
+import merge from 'deepmerge'
+import log from 'loglevel'
 
-export default Pas
+interface IPutioAnalyticsClientOptions {
+  loglevel: log.LogLevelDesc
+}
+
+class PutioAnalyticsClient {
+  public static DEFAULT_OPTIONS: IPutioAnalyticsClientOptions = {
+    loglevel: 'WARN',
+  }
+
+  public options: IPutioAnalyticsClientOptions
+  public logger: log.Logger
+
+  constructor(options: IPutioAnalyticsClientOptions) {
+    this.options = merge(options, {})
+    this.logger = log.noConflict()
+    this.logger.setLevel(this.options.loglevel)
+  }
+}
+
+export default PutioAnalyticsClient
