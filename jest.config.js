b341acbc1680e29132989df02791c58e9141a862
Altay Aydemir <altay.aydemir@gmail.com>
2020-01-18 10:43:58 +0100

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 const { defaults } = require('jest-config')
 
 module.exports = {
-  transform: {
-    '.(ts|tsx)': 'ts-jest',
-  },
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  rootDir: 'src',
+  preset: 'ts-jest',
+  testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, 'dist'],
 }
